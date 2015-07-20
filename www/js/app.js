b4b4179acb4337dfcaba6593bdee22e9724fef17
nicodel <nicolas.delebecque@gmail.com>
2015-07-20 10:49:55 +0200

    adding application launcher and change html title

---
+++ www/js/app.js
@@ -0,0 +1,8 @@
+/* jshint strict: true, browser: true */
+
+var app = app || {};
+
+document.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+  console.log('launching');
+}, false);
