2f10e61361c5f9d7528881b8f8e50994668187e8
nicodel <nicolas.delebecque@gmail.com>
2015-08-14 15:00:14 +0200

    adding charts library

--- client/js/views/session.js
+++ client/js/views/session.js
@@ -2,7 +2,7 @@
 'use strict';
 var Backbone            = require('../lib/exoskeleton');
 var Template            = require('microtemplates');
-var Chart               = require('../lib/Chart');
+var dc                  = require('../lib/dc');
 
 // var SessionsCollection  = require('../collections/sessions');
 var Preferences         = require('../models/preferences');
@@ -70,23 +70,7 @@ var SessionView = Backbone.NativeView.extend({
     /*
      * building the Altitude graph
      */
-    var ctx = document.getElementById('session-alt-graph').getContext('2d');
-    var data = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
-      datasets: [
-        {
-          label : 'Altitude',
-          fillColor: "rgba(220,220,220,0.2)",
-          strokeColor: "rgba(220,220,220,1)",
-          pointColor: "rgba(220,220,220,1)",
-          pointStrokeColor: "#fff",
-          pointHighlightFill: "#fff",
-          pointHighlightStroke: "rgba(220,220,220,1)",
-          data: [65, 59, 80, 81, 56, 55, 40]
-        }
-      ]
-    };
-    new Chart(ctx).Line(data);
+    var alt_line = dc.lineChart('#session-alt-grap');
 
     return this;
   },
