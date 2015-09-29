14a390c373efcb8e015980ffe589d3b7f67eb544
nicodel <nicolas.delebecque@gmail.com>
2015-09-29 21:35:57 +0200

    extending factories for all activities

---
+++ client/js/factories/activities/weight_act.js
@@ -0,0 +1,9 @@
+/* jshint strict: true,  node: true */
+'use strict';
+module.exports = {
+  model                   : require('../models/body'),
+  new_view                : require('../views/new_1'),
+  summary_view_dashboard  : require('../views/dashboard_summary_1'),
+  summary_view_sessions   : require('../views/sessions_summary_1'),
+  detailled_view          : require('../views/detailled_1')
+};
--- client/js/factories/factory.js
+++ client/js/factories/factory.js
@@ -2,17 +2,29 @@
 'use strict';
 
 var activities = {
-  running : require('./activities/running')
+  climbing          : require('./activities/climbing'),
+  mountain_biking   : require('./activities/mountain_biking'),
+  paddling          : require('./activities/paddling'),
+  racing            : require('./activities/racing'),
+  regular_biking    : require('./activities/regular_biking'),
+  running           : require('./activities/running'),
+  skiing            : require('./activities/skiing'),
+  time_trial_biking : require('./activities/time_trial_biking'),
+  trekking          : require('./activities/trekking'),
+  walking           : require('./activities/walking'),
+  weight_act        : require('./activities/weight_act'),
+  swimming          : require('./activities/swimming')
 };
 
 var Factory = (function() {
   var getModel = function(activity, options) {
+    console.log('FACTORY - get model for', activity);
     var Model = activities[activity].model;
     return Model ? new Model(options) : null;
   };
   var getNewView = function(model) {
-    // var activity = model.get('activity');
-    var View = activities['running'].new_view;
+    console.log('FACTORY - display new session view for', model);
+    var View = activities[model.get('activity')].new_view;
     return new View({
       model: model
     });
@@ -25,7 +37,7 @@ var Factory = (function() {
     });
   };
   var getSessionsSummaryView = function(model) {
-  var View = activities[model.get('activity')].summary_view_sessions;
+    var View = activities[model.get('activity')].summary_view_sessions;
     console.log('FACTORY - display sessions summary view for', View);
     return new View({
       model: model
